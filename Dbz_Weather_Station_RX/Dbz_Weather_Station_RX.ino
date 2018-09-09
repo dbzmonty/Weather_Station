94e7b30f1f7471a0bddbcfb549d1a32276133538
Dobozi Andras <dbzmonty@gmail.com>
2018-09-09 13:49:19 +0200

    Code blocks put to methods, refreshing display solved, next step is to send datas to IOT.

--- Dbz_Weather_Station_RX/Dbz_Weather_Station_RX.ino
+++ Dbz_Weather_Station_RX/Dbz_Weather_Station_RX.ino
@@ -15,11 +15,8 @@
 //Variables
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 float msg[3];
-float in_temp;
-float in_hum;
-float out_temp;
-float out_hum;
-float pres;
+float in_temp, in_hum, out_temp, out_hum, pres;
+float in_temp_changed, in_hum_changed, out_temp_changed, out_hum_changed, pres_changed;
 
 //ETC
 Adafruit_SSD1306 display(OLED_RESET);
@@ -43,6 +40,50 @@ void setup() {
 
 void loop() {
 
+  CollectingValues();
+  SendDatasToWifi();
+  if ( CheckDifferences() ) { RefreshDisplay(); }
+  delay(2000);
+}
+
+void CollectingValues() {
+
+  //IN values
+  in_temp = dht.getTemperature();
+  in_hum = dht.getHumidity();
+  
+  //OUT values
+  if( radio.available()) 
+  {    
+    radio.read( &msg, sizeof(msg) );
+    out_temp = msg[0];
+    out_hum = msg[1];
+    pres = msg[2];    
+  }
+}
+
+void SendDatasToWifi() {
+
+  
+}
+
+boolean CheckDifferences() {
+
+  if (  (in_temp_changed  != in_temp)  ||
+        (in_hum_changed   != in_hum)   ||
+        (out_temp_changed != out_temp) ||
+        (out_hum_changed  != out_hum)  ||
+        (pres_changed     != pres) )    
+    return true;    
+  else 
+    return false;
+}
+
+void RefreshDisplay() {
+
+  //Clear the display
+  display.clearDisplay();
+  
   //Draw the lines
   display.drawLine(0, 0, 63, 0, WHITE);
   display.drawLine(63, 0, 63, 127, WHITE);
@@ -51,56 +92,20 @@ void loop() {
   display.drawLine(0, 42, 63, 42, WHITE);
   display.drawLine(0, 84, 63, 84, WHITE);
   display.drawLine(31, 0, 31, 84, WHITE);
+  
+  //Display the numbers
+  display.setCursor(5, 15);
+  display.print(in_temp, 1);
+  display.setCursor(5, 55);
+  display.print(in_hum, 1);
+  display.setCursor(35,15);
+  display.print(out_temp, 1);
+  display.setCursor(35,55);
+  display.print(out_hum, 1);
+  display.setCursor(12,100);
+  display.print(pres, 1);
+  
   display.display();
-
-  if( radio.available())
-  {
-    while (radio.available())
-    {
-      radio.read( &msg, sizeof(msg) );
-      
-      in_temp = dht.getTemperature();
-      in_hum = dht.getHumidity();
-      out_temp = msg[0];
-      out_hum = msg[1];
-      pres = msg[2];      
-      
-      //IN Temp
-      Serial.print("IN Temperature: ");
-      Serial.print(in_temp, 1);
-      Serial.println(" °C");
-      //IN Hum
-      Serial.print("IN Humidity: ");
-      Serial.print(in_hum, 1);
-      Serial.println(" %");
-      //OUT Temp
-      Serial.print("OUT Temperature: ");
-      Serial.print(out_temp, 1);
-      Serial.println(" °C");
-      //OUT Hum
-      Serial.print("OUT Humidity: ");
-      Serial.print(out_hum, 1);
-      Serial.println(" %");
-      //PRES
-      Serial.print("Air Pressure: ");
-      Serial.print(pres, 1);
-      Serial.println(" hPa");
-      Serial.println("**********************");
-
-      //Display the numbers
-      display.setCursor(5, 15);
-      display.print(in_temp, 1);
-      display.setCursor(5, 55);
-      display.print(in_hum, 1);
-      display.setCursor(35,15);
-      display.print(out_temp, 1);
-      display.setCursor(35,55);
-      display.print(out_hum, 1);
-      display.setCursor(12,100);
-      display.print(pres, 1);
-      
-      display.display();
-    }
-  }  
-  delay(100);
 }
+
+
